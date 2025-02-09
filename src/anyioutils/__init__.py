e4c5e62d20c438f2606cb31d8d43c15b3527dc1b
David Brochart <david.brochart@gmail.com>
2025-02-09 22:12:54 +0100

    Add Queue

--- src/anyioutils/__init__.py
+++ src/anyioutils/__init__.py
@@ -3,6 +3,7 @@
 from ._exceptions import CancelledError as CancelledError
 from ._exceptions import InvalidStateError as InvalidStateError
 from ._future import Future as Future
+from ._queue import Queue as Queue
 from ._task import Task as Task
 from ._task import create_task as create_task
 from ._wait import ALL_COMPLETED as ALL_COMPLETED
